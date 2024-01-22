3ab64968cea44260bb9196797f7bcc2985cf11a8
Albert Fit <albert.ioan.fit@gmail.com>
2024-01-22 16:50:57 +0000

    Exploring state updates and callbacks in class components

--- src/App.js
+++ src/App.js
@@ -1,22 +1,40 @@
-import {Component} from 'react'
+import { Component } from "react";
 
-
-import logo from './logo.svg';
-import './App.css'; 
+import logo from "./logo.svg";
+import "./App.css";
 
 class App extends Component {
+  constructor() {
+    super();
 
-  
+    this.state = {
+      name: { firstName: "Albert", lastName: "Fit" },
+      company: "TSS",
+    };
+  }
 
-  render () {
+  render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Hi Albert
+            Hi {this.state.name.firstName} {this.state.name.lastName}, I work at {this.state.company}
           </p>
-          <button>Change Name</button>
+          <button
+            onClick={() => {
+              this.setState((state, props) => {
+                return {
+                  name: {firstName: 'George', lastName: 'Baker'}
+                }
+              }, () => {
+                //optional callback
+                console.log(this.state);
+              });
+            }}
+          >
+            Change Name
+          </button>
         </header>
       </div>
     );
